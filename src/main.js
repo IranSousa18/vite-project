e345984a9b460f121ce051967e783ec387d8ec57
iran <iran.csousa21@gmail.com>
2026-05-25 11:59:21 -0300

    Pagina inical funcionando e cardapio tbm, agora falta mover os itens que forem escolhidos para o carrinho

--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './style.css'
+
+const produtos = [
+    {
+        nome: "Hamburguer",
+        preco: 25
+    },
+
+    {
+        nome: "Batata Frita",
+        preco: 10
+    },
+
+    {
+        nome: "Refrigerante",
+        preco: 5
+    },
+];
+
+const listaProdutos = document.getElementById("listaProdutos");
+let carrinho = [];
+
+produtos.forEach((produto) => {
+    listaProdutos.innerHTML += `
+    <div class="col-md-3 mb-4">
+        <div class="card p--3 h-100">
+            <h3>${produto.nome}</h3>
+            <p class="fs-5">
+                R$ ${produto.preco}
+            </p>
+            <button class="btn btn-success" onclick="adicionarCarrinho('${produto.nome}')">
+                Adicionar
+            </button>
+        </div>
+    </div>
+    `;
+});
+
+function adicionarCarrinho(nomeProduto){
+    const produtoSelecionado = produtos.find((produto) => {
+        return produto.nome === nomeProduto;
+    });
+
+    carrinho.push(produtoSelecionado);
+    console.log(carrinho);
+    alert(`${nomeProduto} adicionado ao carrinho`);
+}
+
+window.adicionarCarrinho = adicionarCarrinho;
